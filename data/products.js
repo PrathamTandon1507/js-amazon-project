ba334c5e7f6dcfafa95905427091c3a33db14ad8
Pratham <prattandon1507@gmail.com>
2024-08-07 11:07:22 +0530

    saved html code using products array, generated html using products array and DOM in amazon.js

--- data/products.js
+++ data/products.js
@@ -16,7 +16,7 @@ const products = [
   },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    image: "images/products/intermediate-composite-basketball.jpg",
+    image: "imagdata/products.jses/products/intermediate-composite-basketball.jpg",
     name: "Intermediate Size Basketball",
     rating: {
       stars: 4,

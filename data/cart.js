92f70ecff24d053d9a15547c9dec3f07c9b786ee
Pratham <prattandon1507@gmail.com>
2024-09-03 01:06:51 +0530

    Added removeCartItem function and made 'delete' interactive in checkout

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{ //let because const cant be reassigned
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 },{
@@ -25,4 +25,14 @@ export function addToCart(productId){
         quantity : quant
       });
     }
+}
+
+export function removeCartItem(productId){
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if(cartItem.productId !== productId){
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
 }

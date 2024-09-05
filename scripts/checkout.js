29cbe08b8d60fef53a4156d191c732129a3e8c67
Pratham <prattandon1507@gmail.com>
2024-09-05 20:59:05 +0530

    Convert promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,16 +2,41 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderItemPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadCartFetch, loadProductsFetch } from "../data/products.js";
 
+async function loadPage(){ //returns a promise (just like fetch)
+    console.log('loading page');
+    await loadProductsFetch();
+    await loadCartFetch();
+    /*
+    await new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+    */
+    renderOrderSummary();
+    renderItemPaymentSummary();
+    return 'loaded';
+}
+
+loadPage(); 
+
+/* TO PRINT WHATEVER STATMEENT WAS RETURNED, WE CAN FURTHER ACCESS THE RETURNED PROMISE
+loadPage().then((value) => {
+    console.log(value);
+});
+*/
+
+/*
 Promise.all([ //RUNS BOTH PROMISES AT THE SAME TIME
     loadProductsFetch()
     ,
     loadCartFetch()
-]).then((values) => { //values to get both parameters in the form of an array
+]).then((values) => { //values to get both parameters in the form of an array [not necessary]
     renderOrderSummary();
     renderItemPaymentSummary();
 });
 //LOAD PRODUCTS FROM API, THEN LOAD CART FROM API, THEN DISPLAY ORDER AND CART ONCE DATA HAS BEEN LOADED FROM BACKEND
-
+*/
 
 /*
 new Promise((resolve) => { //its like wait(), we wait till the process is finished then the code continues from resolve
